2e63b92cd5e60130a177243492f674f4ef213a10
dong <dongerqian@gmail.com>
2025-12-27 10:37:22 +0800

    删除后端文件

--- sscm-backend/admin/src/main/java/com/kakarote/admin/oceanengine/mapper/OeSaleUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kakarote.admin.oceanengine.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kakarote.admin.oceanengine.entity.OeSaleUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OeSaleUserMapper extends BaseMapper<OeSaleUser> {
-}
